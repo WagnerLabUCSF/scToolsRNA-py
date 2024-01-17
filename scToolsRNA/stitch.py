88ad13971fe9c7870c015ac8aaeba1ca5632813d
danwagnerlab <38510695+wagnerde@users.noreply.github.com>
2024-01-17 13:48:19 -0800

    Update stitch.py

--- scToolsRNA/stitch.py
+++ scToolsRNA/stitch.py
@@ -3,20 +3,22 @@ import pandas as pd
 import scanpy as sc
 import numpy as np
 import scipy
+import logging
+from contextlib import contextmanager
 from .dimensionality import *
 from .workflows import *
 
+@contextmanager
+def all_logging_disabled(highest_level=logging.CRITICAL):
+    previous_level = logging.root.manager.disable
+    logging.disable(highest_level)
+    try:
+        yield
+    finally:
+        logging.disable(previous_level)
 
-# (from ingest)
-#def project_to_pca(adata, n_pcs=None):
-#    X = adata.X
-#    X = X.toarray() if issparse(X) else X.copy()
-#    X = X[:, adata_ref.var["highly_variable"]]
-#    X_pca = np.dot(X, adata.varm["PCs"][:, :n_pcs])
-#    return X_pca
 
-
-def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric='correlation', vscore_min_pctl=95, vscore_filter_method=None):
+def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric='correlation', vscore_min_pctl=95, vscore_filter_method=None, method='forward'):
 
   # Determine the # of timepoints in adata
   timepoint_list = np.unique(adata.obs[timepoint_obs])
@@ -38,35 +40,48 @@ def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric
   with warnings.catch_warnings():
     warnings.simplefilter('ignore')
     for n in range(n_stitch_rounds):
-      print('Stitching Timepoints:', timepoint_list[n], '-', timepoint_list[n+1])
 
       # Specify individual adatas for the two timepoints in this round
       adata_t1 = adata_list[n].copy()
       adata_t2 = adata_list[n+1].copy()
 
-      # Normalize the two timepoints separately
+      # Normalize the two adata objects separately
       pp_raw2norm(adata_t1)
       pp_raw2norm(adata_t2)
 
-      # Define variable genes and nPCs for t2
-      get_variable_genes(adata_t2, batch_key=batch_obs, filter_method=vscore_filter_method, min_vscore_pctl=vscore_min_pctl)
-      nPCs_test_use = np.min([300, np.sum(adata_t2.var.highly_variable)-1])
-      get_significant_pcs(adata_t2, n_iter=1, nPCs_test = nPCs_test_use, show_plots=False, verbose=False)
-      print(np.sum(np.sum(adata_t2.var['highly_variable'])), adata_t2.uns['n_sig_PCs'])
+      # Set directionality of time projections
+      if method=='forward':
+        adata_ref = adata_t2
+        adata_project = adata_t1
+        arrow_str = '->'
+      elif method=='reverse':
+        adata_ref = adata_t1
+        adata_project = adata_t2
+        arrow_str = '<-'
+      
+      # Perform projections
+      print('Stitching Timepoints:', timepoint_list[n], arrow_str, timepoint_list[n+1])
 
-      # Get a pca embedding for t2
-      sc.pp.pca(adata_t2, n_comps=adata_t2.uns['n_sig_PCs'], zero_center=True)
-      sc.pp.neighbors(adata_t2, n_neighbors=n_neighbors, n_pcs=adata_t2.uns['n_sig_PCs'], metric=distance_metric, use_rep='X_pca')
+      # Define variable genes and nPCs for adata_ref
+      get_variable_genes(adata_ref, batch_key=batch_obs, filter_method=vscore_filter_method, min_vscore_pctl=vscore_min_pctl)
+      nPCs_test_use = np.min([300, np.sum(adata_ref.var.highly_variable)-1])
+      get_significant_pcs(adata_ref, n_iter=1, nPCs_test = nPCs_test_use, show_plots=False, verbose=False)
+      print(np.sum(np.sum(adata_ref.var['highly_variable'])), adata_ref.uns['n_sig_PCs'])
 
-      # Project t1 into the pca subspace defined for t2
-      sc.tl.ingest(adata_t1, adata_t2, embedding_method='pca')
+      # Get a pca embedding for adata_ref
+      sc.pp.pca(adata_ref, n_comps=adata_ref.uns['n_sig_PCs'], zero_center=True)
+      sc.pp.neighbors(adata_ref, n_neighbors=n_neighbors, n_pcs=adata_ref.uns['n_sig_PCs'], metric=distance_metric, use_rep='X_pca')
 
-      # Concatenate the pca projections for t1 & t2
+      # Embed adata_project into the pca subspace defined by adata_ref
+      sc.tl.ingest(adata_project, adata_ref, embedding_method='pca')
+        
+      # Concatenate the pca projections for both timepoints in chronological order (not projection order)
       adata_t1t2 = adata_t1.concatenate(adata_t2, batch_categories=['t1', 't2'])
 
       # Generate a t1-t2 neighbor graph in the joint pca space
       if True: # include Harmony batch correction
-        sc.external.pp.harmony_integrate(adata_t1t2, batch_obs, basis='X_pca', adjusted_basis='X_pca_harmony', max_iter_harmony=20, verbose=False)
+        with all_logging_disabled():
+          sc.external.pp.harmony_integrate(adata_t1t2, batch_obs, basis='X_pca', adjusted_basis='X_pca_harmony', max_iter_harmony=20, verbose=False)
         sc.pp.neighbors(adata_t1t2, n_neighbors=n_neighbors, metric=distance_metric, use_rep='X_pca_harmony')
         del adata_t1t2.uns['neighbors']['params']['use_rep']
       else: # version without Harmony
