315be38db7275a5fba044dd84b0fc676b2ed2049
danwagnerlab <38510695+wagnerde@users.noreply.github.com>
2024-02-09 17:01:45 -0800

    Update stitch.py

--- scToolsRNA/stitch.py
+++ scToolsRNA/stitch.py
@@ -67,7 +67,7 @@ def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric
 
   # Sort the cells in adata by timepoint
   time_sort_index = adata.obs[timepoint_obs].sort_values(inplace=False).index
-  adata = adata[time_sort_index,:]#.copy()
+  adata = adata[time_sort_index,:] #.copy()
 
   # Generate a list of individual timepoint adatas
   adata_list = []
@@ -99,8 +99,8 @@ def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric
       if verbose: print('Stitching Timepoints:', timepoint_list[n], arrow_str, timepoint_list[n+1])
 
       # Specify the reference and projection adatas this round
-      adata_t1 = adata_list[n]#.copy()
-      adata_t2 = adata_list[n+1]#.copy()
+      adata_t1 = adata_list[n] #.copy()
+      adata_t2 = adata_list[n+1] #.copy()
       if method=='forward':
         adata_ref = adata_t2
         adata_prj = adata_t1
