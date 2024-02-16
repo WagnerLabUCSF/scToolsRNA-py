b8aff231244496416b24afa78ea8cf659c4600ba
Dan Wagner <38510695+wagnerde@users.noreply.github.com>
2024-02-15 17:09:53 -0800

    Update stitch.py

--- scToolsRNA/stitch.py
+++ scToolsRNA/stitch.py
@@ -58,6 +58,7 @@ def get_connectivities_from_dist_csr(D_csr, n_neighbors):
   return connectivities
 
 
+
 def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric='correlation', vscore_min_pctl=95, vscore_filter_method=None, method='forward', use_harmony=True, max_iter_harmony=20, verbose=True):
 
   # Determine the # of timepoints in adata
@@ -91,7 +92,7 @@ def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric
   stitch_nSigPCs = []
   stitch_nBatches = []
   coo_shape = (len(adata), len(adata))
-  X_d_stitch_combined = sp.coo_matrix(coo_shape)
+  X_d_stitch_combined = scipy.sparse.coo_matrix(coo_shape)
 
   # Get neighbor graph for each stitch_round (each timepoint pair)
   with warnings.catch_warnings():
@@ -170,15 +171,13 @@ def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric
       #X_d_stitch_rows = np.concatenate((X_d_stitch_rows, X_d_coo.row + base_counter))
       #X_d_stitch_cols = np.concatenate((X_d_stitch_cols, X_d_coo.col + base_counter))
       #X_d_stitch_data = np.concatenate((X_d_stitch_data, X_d_coo.data))
-
-      X_d_coo = sp.coo_matrix((X_d_coo.data, (X_d_coo.row + base_counter, X_d_coo.col + base_counter)), shape=coo_shape)
-      X_d_stitch_combined = sp.vstack([X_d_stitch_combined, X_d_coo])
+      X_d_coo = scipy.sparse.coo_matrix((X_d_coo.data, (X_d_coo.row + base_counter, X_d_coo.col + base_counter)), shape=coo_shape)
+      X_d_stitch_combined = scipy.sparse.vstack([X_d_stitch_combined, X_d_coo])
 
       # Increment base_counter by the # of cells in adata_t1
       base_counter += len(adata_t1)
 
   # Assemble the full STITCH graph as a COO matrix
-  del adata.obsp['distances']
   #adata.obsp['distances'] = scipy.sparse.coo_matrix((X_d_stitch_data, (X_d_stitch_rows, X_d_stitch_cols)), shape=(len(adata), len(adata))).tocsr()
   adata.obsp['distances'] = X_d_stitch_combined.tocsr()
 
@@ -194,3 +193,4 @@ def stitch(adata, timepoint_obs, batch_obs=None, n_neighbors=15, distance_metric
                                 'stitch_nHVgenes': stitch_nHVgenes, 'stitch_nSigPCs': stitch_nSigPCs, 'stitch_nBatches': stitch_nBatches}
  
   return adata
+
