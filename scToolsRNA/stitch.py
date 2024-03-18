424076b9acf93096c2b04ff2184a06c82a3a971c
danwagnerlab <38510695+wagnerde@users.noreply.github.com>
2024-03-18 16:26:16 -0700

    Update stitch.py

--- scToolsRNA/stitch.py
+++ scToolsRNA/stitch.py
@@ -81,6 +81,9 @@ def get_stitch_dims(adata, timepoint_obs, batch_obs=None, vscore_min_pctl=95, vs
   # Initialize results lists
   stitch_nHVgenes = []
   stitch_HVgene_flags = []
+  stitch_HVgene_vscores = []
+  stitch_HVgene_ff = []
+  stitch_HVgene_mu = []
   stitch_nSigPCs = []
   stitch_PCs = []
   stitch_PC_loadings = []
@@ -111,6 +114,9 @@ def get_stitch_dims(adata, timepoint_obs, batch_obs=None, vscore_min_pctl=95, vs
 
       stitch_nHVgenes.append(this_round_nHVgenes)
       stitch_HVgene_flags.append(adata_tmp.var['highly_variable'])
+      stitch_HVgene_vscores.append(adata_tmp.var['vscore'])
+      stitch_HVgene_ff.append(adata_tmp.var['ff_gene'])
+      stitch_HVgene_mu.append(adata_tmp.var['mu_gene'])
       stitch_nSigPCs.append(this_round_nSigPCs)
       stitch_PCs.append(adata_tmp.varm['PCs'])
       stitch_PC_loadings.append(adata_tmp.obsm['X_pca'])
@@ -119,7 +125,8 @@ def get_stitch_dims(adata, timepoint_obs, batch_obs=None, vscore_min_pctl=95, vs
   adata.uns['stitch_dims'] = {'timepoint_obs': timepoint_obs, 'batch_obs': batch_obs, 'vscore_min_pctl': vscore_min_pctl, 
                               'vscore_filter_method': vscore_filter_method, 'stitch_timepoints': timepoint_list, 
                               'stitch_n_timepoints': n_timepoints, 'stitch_nHVgenes': stitch_nHVgenes, 
-                              'stitch_HVgene_flags': stitch_HVgene_flags, 
+                              'stitch_HVgene_flags': stitch_HVgene_flags, 'stitch_HVgene_vscores': stitch_HVgene_vscores, 
+                              'stitch_HVgene_ff': stitch_HVgene_ff, 'stitch_HVgene_mu': stitch_HVgene_mu, 
                               'stitch_PCs': stitch_PCs, 'stitch_PC_loadings': stitch_PC_loadings, 'stitch_nSigPCs': stitch_nSigPCs}
  
   return adata
