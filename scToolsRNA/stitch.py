a6ed8310fe9594ce69da88c77818d17d8b597b2d
danwagnerlab <38510695+wagnerde@users.noreply.github.com>
2024-03-28 17:20:08 -0700

    Update stitch.py

--- scToolsRNA/stitch.py
+++ scToolsRNA/stitch.py
@@ -395,6 +395,7 @@ def stitch_get_graph(adata, timepoint_obs, batch_obs=None, n_neighbors=15, dista
       adata_t1.uns['n_sig_PCs'] = nSigPCs[n]
       adata_t1.obsm['X_pca'] = X_pca[n].copy()
       adata_t1.varm['PCs'] = PCs[n].copy()
+      adata_t1.uns['pca'] = {}
       adata_t1.uns['pca']['params'] = {'zero_center': True, 'use_highly_variable': True}
       
       adata_t2 = adata[obsnames[n+1],:].copy()
@@ -402,6 +403,7 @@ def stitch_get_graph(adata, timepoint_obs, batch_obs=None, n_neighbors=15, dista
       adata_t2.uns['n_sig_PCs'] = nSigPCs[n+1]
       adata_t2.obsm['X_pca'] = X_pca[n+1].copy()
       adata_t2.varm['PCs'] = PCs[n+1].copy()
+      adata_t2.uns['pca'] = {}
       adata_t2.uns['pca']['params'] = {'zero_center': True, 'use_highly_variable': True}
       
       # Normalize 
