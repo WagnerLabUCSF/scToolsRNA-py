ef36a2d5a665986de0f8f541a173b7a31c174c0f
Dan Wagner <38510695+wagnerde@users.noreply.github.com>
2024-03-28 22:04:42 -0700

    Update stitch.py

--- scToolsRNA/stitch.py
+++ scToolsRNA/stitch.py
@@ -391,18 +391,18 @@ def stitch_get_graph(adata, timepoint_obs, batch_obs=None, n_neighbors=15, dista
       
       # Specify adata_t1 and adata_t2 for this round
       adata_t1 = adata[obsnames[n],:].copy()
-      adata_t1.var['highly_variable'] = hvflags[n].copy()
+      adata_t1.var['highly_variable'] = hvflags[n]
       adata_t1.uns['n_sig_PCs'] = nSigPCs[n]
-      adata_t1.obsm['X_pca'] = X_pca[n].copy()
-      adata_t1.varm['PCs'] = PCs[n].copy()
+      adata_t1.obsm['X_pca'] = X_pca[n]
+      adata_t1.varm['PCs'] = PCs[n]
       adata_t1.uns['pca'] = {}
       adata_t1.uns['pca']['params'] = {'zero_center': True, 'use_highly_variable': True}
       
       adata_t2 = adata[obsnames[n+1],:].copy()
-      adata_t2.var['highly_variable'] = hvflags[n+1].copy()
+      adata_t2.var['highly_variable'] = hvflags[n+1]
       adata_t2.uns['n_sig_PCs'] = nSigPCs[n+1]
-      adata_t2.obsm['X_pca'] = X_pca[n+1].copy()
-      adata_t2.varm['PCs'] = PCs[n+1].copy()
+      adata_t2.obsm['X_pca'] = X_pca[n+1]
+      adata_t2.varm['PCs'] = PCs[n+1]
       adata_t2.uns['pca'] = {}
       adata_t2.uns['pca']['params'] = {'zero_center': True, 'use_highly_variable': True}
       
